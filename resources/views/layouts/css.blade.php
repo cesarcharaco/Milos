39d355779821e08d8eebe6864d66b827074cae22
antonileon <antonijleon@gmail.com>
2019-08-05 22:56:42 -0430

    vista de conductores

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,6 +1,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Ela Admin - HTML5 Admin Template</title>
+    <title>MILOS</title>
     <meta name="description" content="Ela Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -21,3 +21,5 @@
 
     <link href="{{ asset('ElaAdmin/assets/css/weather-icons.css') }}" rel="stylesheet" />
     <link href="{{ asset('ElaAdmin/assets/css/fullcalendar.min.css') }}" rel="stylesheet" />
+
+    <link rel="stylesheet" href="{{ asset('ElaAdmin/assets/css/lib/datatable/dataTables.bootstrap.min.css') }}">
