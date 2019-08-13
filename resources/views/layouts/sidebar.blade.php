520fd4a27e5acb5e858cfa694cd205c97e89bf29
antonileon <antonijleon@gmail.com>
2019-08-13 15:49:38 -0430

    cambion en sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -21,6 +21,13 @@
                     <li>
                         <a href="{{ route('recepciones.index') }}"> <i class="menu-icon ti-shift-left"></i>Llegada de Camiones </a>
                     </li>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-bar-chart-o"></i>Reportes</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="fa fa-table"></i><a href="#">Despachos y Recepciones</a></li>
+                            <li><i class="fa fa-table"></i><a href="#">Estadísticas Generales</a></li>
+                        </ul>
+                    </li>
                     @if(\Auth::getUser()->user_type=="Admin")
                     <li class="menu-title">Configuraciones</li><!-- /.menu-title -->
                     <li>
